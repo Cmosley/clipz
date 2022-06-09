c733fee87abe1770545fd7eec53568b796498d9d
cmosley <cmosley@beholdmedia.com>
2022-06-09 14:52:32 -0500

    modal service

---
+++ clipz/src/app/shared/modal/modal.component.ts
@@ -0,0 +1,22 @@
+import { ModalService } from './../../services/modal.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  //providers: [ModalService]
+})
+export class ModalComponent implements OnInit {
+
+  constructor(public modal: ModalService) { 
+  }
+
+  ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.modal.toggleModal()
+  }
+
+}
