0246f0e1b60be182a1f3c390d22c20f439a01350
cmosley <cmosley@beholdmedia.com>
2022-06-10 12:50:43 -0500

    modal complete, tabs comp.

--- clipz/src/app/shared/modal/modal.component.ts
+++ clipz/src/app/shared/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { ModalService } from './../../services/modal.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -8,15 +8,20 @@ import { Component, OnInit } from '@angular/core';
   //providers: [ModalService]
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = ''
 
-  constructor(public modal: ModalService) { 
+  constructor(
+      public modal: ModalService,
+      public el: ElementRef
+    ) { 
   }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal() {
-    this.modal.toggleModal()
+    this.modal.toggleModal(this.modalID)
   }
 
 }
