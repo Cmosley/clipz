f16fcbb41b1ff350b5d338c4060f8cf2c1490ce9
cmosley <cmosley@beholdmedia.com>
2022-06-10 23:19:41 -0500

    ngx-mask

--- clipz/src/app/shared/input/input.component.ts
+++ clipz/src/app/shared/input/input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
@@ -10,6 +11,7 @@ export class InputComponent implements OnInit {
   @Input() control: FormControl = new FormControl();
   @Input() type = 'text'
   @Input() placeholder = ''
+  @Input() format = ''
 
   constructor() {}
 
--- clipz/src/app/user/register/register.component.ts
+++ clipz/src/app/user/register/register.component.ts
@@ -7,21 +7,21 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  name = new FormControl('', [
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [
     Validators.required,
-    Validators.minLength(3)
+    Validators.min(18),
+    Validators.max(120),
   ]);
-  email = new FormControl('', [
-    Validators.required, 
-    Validators.email
-  ]); 
-  age = new FormControl('',[
-    Validators.required, 
-    Validators.min(18), 
-    Validators.max(120)
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
   ]);
-  password = new FormControl('', Validators.required);
-  confirmPassword = new FormControl('', Validators.required);
+  confirmPassword = new FormControl('', [
+    Validators.required,
+    
+  ]);
   phoneNumber = new FormControl('');
 
   registerForm = new FormGroup({
@@ -30,7 +30,6 @@ export class RegisterComponent {
     age: this.age,
     password: this.password,
     confirmPassword: this.confirmPassword,
-    phoneNumber: this.phoneNumber
+    phoneNumber: this.phoneNumber,
   });
-
 }
