794e679c7011efccc9971d897223ab673e88e996
cmosley <cmosley@beholdmedia.com>
2022-06-15 14:41:16 -0500

    firebase auth setup

--- clipz/src/environments/environment.prod.ts
+++ clipz/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCrcCgjDviCquuRePvvgeEU5pgV-bR7Fic",
+    authDomain: "clipz-16072.firebaseapp.com",
+    projectId: "clipz-16072",
+    storageBucket: "clipz-16072.appspot.com",
+    appId: "1:930520726978:web:0c2b5cd30257c9480b2a5b"
+  },
 };
